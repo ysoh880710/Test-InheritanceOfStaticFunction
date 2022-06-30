6c0fe6d171d1831f19605c1a20223128d89ede8f
ysoh880710 <ysoh880710@gmail.com>
2022-06-30 15:32:20 +0900

    Create project

---
+++ InheritanceOfStaticFunction/InheritanceOfStaticFunction.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+class CA
+{
+private:
+public:
+
+	CA(){}
+	~CA(){}
+
+	static void Function0()
+	{
+		std::wcout << L"Function0" << std::endl;
+	}
+};
+
+class CB : public CA
+{
+private:
+public:
+
+	CB(){}
+	~CB(){}
+};
+
+int main()
+{
+	CB::Function0();
+	return 0;
+}
